5b1b8536caa2995408f650386f74c526af03b2cd
oswi307 <110707251+oswi307@users.noreply.github.com>
2025-05-08 18:23:39 -0500

    C-5

--- app/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/notes/[id]/page.tsx (Server Component)
-import NoteClient from "./NoteClient";
-
-
-
-export default function Page({ params }: { params: { id: string } })  {
-  return <NoteClient id={params.id} />
-}
