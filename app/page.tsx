fcb353b9f677c74e7e66c24dc4b8822290fe2e5f
oswi307 <110707251+oswi307@users.noreply.github.com>
2025-05-08 18:08:29 -0500

    C-2

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Notes from "@/components/tests/Notes";
-import Tiptap from '../components/Tiptap'
+
 
 export default function Home() {
   return (
     <main>
       <h1>Bienvenido a courseCreator</h1>
       <Notes />
-      <Tiptap/>
+
   
     </main>
   );
